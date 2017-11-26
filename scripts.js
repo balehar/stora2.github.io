76a753985bab4378dfb875fed59762379381c779
balehar <baldurelfar@gmail.com>
2017-11-26 21:03:31 +0000

    load json

---
+++ scripts.js
@@ -0,0 +1,11 @@
+
+class Spilari {
+  constructor(container) {
+    this.container = container;
+    this.load();
+  }
+
+  load() {
+    const http = new XMLHttpRequest();
+    request.open('GET', './videos.json', true);
+}
